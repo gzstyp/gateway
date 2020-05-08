889c5d4096f1a08d86a111d99aef4c106f52ee67
gzstyp <444141300@qq.com>
2020-05-08 22:01:21 +0800

    记得开启redis

--- gateway/src/main/java/com/fwtai/GateWay.java
+++ gateway/src/main/java/com/fwtai/GateWay.java
@@ -2,7 +2,6 @@ package com.fwtai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +10,6 @@ import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableDiscoveryClient
 public class GateWay{
 
     public static void main(final String[] args){
@@ -25,7 +23,7 @@ public class GateWay{
         return new KeyResolver(){
             @Override
             public Mono<String> resolve(final ServerWebExchange exchange){
-                //System.out.println(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+                System.out.println(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
                 return Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
             }
         };
