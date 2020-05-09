a22b89bec67bed09417372dcc0553a78d52e0823
gzstyp <444141300@qq.com>
2020-05-09 13:27:52 +0800

    添加了响应到客户端数据

--- gateway/src/main/java/com/fwtai/tool/LoginFilter.java
+++ gateway/src/main/java/com/fwtai/tool/LoginFilter.java
@@ -35,6 +35,29 @@ public class LoginFilter implements GlobalFilter, Ordered{
             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);//此时网页显示 该网页无法正常运作,同时会有这样的 HTTP ERROR 401 提示
             return exchange.getResponse().setComplete();
         }*/
+
+        /*ServerHttpResponse response = exchange.getResponse();
+        final String json = "{\"code\":\"200\",\"msg\",\"操作成功\"}";
+        response.writeWith(Mono.just(json));
+
+        ServerHttpResponse response = exchange.getResponse();
+        Map<Object, Object> map = Maps.newHashMap();
+        map.put("code", 401);
+        map.put("message", "非法请求！");
+        map.put("cause", "Token not is null");
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            byte[] bytes = mapper.writeValueAsBytes(map);
+            // 输出错误信息到页面
+            DataBuffer buffer = response.bufferFactory().wrap(bytes);
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            response.getHeaders().add("Content-Type", "application/json;charset=UTF-8");
+            return response.writeWith(Mono.just(buffer));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }*/
+
         return chain.filter(exchange);//如果不符合就放行
     }
 }
